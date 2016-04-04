9cef46529979823b62c0cbb1bf14bc9e827090c3
Austin Minnon and Ramon de la Cruz <whi7ee@gmail.com>
2016-04-04 10:04:13 -0700

    add inital files

---
+++ app/models/listing.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  body: DS.attr(),
+  user: DS.attr(),
+  email: DS.attr(),
+  date: DS.attr(),
+  category: belongsTo('category', {async: true})
+});
